8dabda5d895b2f9478bb6d3bd244585de91c7a44
BilalUsmanDigitalWaze <bilal.digitalwaze@gmail.com>
2021-04-06 20:27:20 +0500

    solved issues

--- app/routes.js
+++ app/routes.js
@@ -4,4 +4,7 @@ module.exports = function (app) {
   // home
   app.use("/", require("./controllers/user"));
   app.use("/", require("./controllers/referrals"));
+  app.use("/", require("./controllers/doctor"));
+  app.use("/", require("./controllers/patient"));
+  app.use("/", require("./controllers/appointment"));
 };
